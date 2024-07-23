040a50ff05965a511ee7bfca71d93963ee34afff
Shakeel23 <sshakeel.ahmadd@gmail.com>
2024-07-23 22:19:13 +0530

    FirstCommitApp

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,26 @@
+﻿using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API;
+
+[Controller]
+[Route("api/[controller]")]    //api/Users
+public class UsersController (DataContext context):ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    {
+        var Users= await context.Users.ToListAsync();
+        return Users;
+    }
+
+    [HttpGet("{id}")]   //api/Users/1
+    public async Task<ActionResult<AppUser>> GetUser(int id)
+    {
+        var user=await context.Users.FindAsync(id);
+        if(user==null) return NotFound();
+
+        return user;
+    }
+}
