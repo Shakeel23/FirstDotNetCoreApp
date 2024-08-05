eb881fd839c8ce4cc29e11534a144c5cfa486706
Shakeel23 <sshakeel.ahmadd@gmail.com>
2024-08-05 15:51:23 +0530

    End of section 5

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -5,7 +5,7 @@ namespace API.Extensions;
 
 public static class ApplicationServiceExtensions
 {
-    public static IServiceCollection AddApllicationServices(this IServiceCollection services, IConfiguration config)
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
     {
         services.AddControllers();
         services.AddDbContext<DataContext>(opt =>
--- API/Program.cs
+++ API/Program.cs
@@ -4,7 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddApllicationServices (builder.Configuration);
+builder.Services.AddApplicationServices (builder.Configuration);
 builder.Services.AddIdentityService(builder.Configuration);
 var app = builder.Build();
 
